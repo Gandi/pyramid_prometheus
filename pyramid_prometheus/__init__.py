5de465d617cb8ddf3d6a0cb0ab822d39b337f91b
Brian Sutherland <brian@vanguardistas.net>
2016-03-22 09:55:12 +0100

    reduce dimensionality of latency measure

--- pyramid_prometheus/__init__.py
+++ pyramid_prometheus/__init__.py
@@ -3,7 +3,7 @@ import time
 from prometheus_client import start_http_server, Histogram, Counter
 from pyramid.tweens import EXCVIEW
 
-_pyramid_request_latency = Histogram('pyramid_request_latency', 'Latency of requests', ['method', 'route', 'status'])
+_pyramid_request_latency = Histogram('pyramid_request_latency', 'Latency of requests', ['route'])
 _pyramid_request_total = Counter('pyramid_requests_total', 'HTTP Requests', ['method', 'route', 'status'])
 
 def tween_factory(handler, registry):
@@ -20,7 +20,7 @@ def tween_factory(handler, registry):
             else:
                 route_name = request.matched_route.name
             label_values = (request.method, route_name, status)
-            _pyramid_request_latency.labels(*label_values).observe(time.time() - start)
+            _pyramid_request_latency.labels(route_name).observe(time.time() - start)
             _pyramid_request_total.labels(*label_values).inc()
     return tween
 
